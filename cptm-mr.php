f49eb12c039d5702f85f09dd7ffe8144e389c9ed
Mahatab Hossain <mahatabricky@users.noreply.github.com>
2019-11-26 18:09:56 +0600

    Setup Plugin header

---
+++ cptm-mr.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Custom Post Type Manager
+ *
+ * @package           mahatab/cptm-mr
+ * @author            Mahatab Hossain
+ * @copyright         2019 Mahatab Hossain
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Custom Post Type Manager
+ * Plugin URI:        https://mahatabricky.com/custom-post-type-manager
+ * Description:       Description of the plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Mahatab Hossain
+ * Author URI:        https://mahatabricky.com
+ * Text Domain:       cptm-mr
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+?>
