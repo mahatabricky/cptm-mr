4e709f1f3996d4ccfaf52ae5ac2ca5ea58c84ea9
Mahatab Hossain <mahatabricky@users.noreply.github.com>
2020-01-05 22:15:11 +0600

    format code

--- Inc/Callbacks/CptCallbacks.php
+++ Inc/Callbacks/CptCallbacks.php
@@ -1,14 +1,27 @@
 <?php
+
 /**
  * @package cptm-mr
  */
+
 namespace Cptmmr\Callbacks;
 
+/**
+ *  Handle Custom post type callbacks
+ * @category Callbacks
+ * @author   <mahatabricky@gmail.com>
+ * 
+ */
 
-class CptCallbacks {
+class CptCallbacks
+{
 
-
-    public function cptSanitize( $input )
+    /**
+     * Sanitize user input
+     * @param array $input
+     * @return void
+     */
+    public function cptSanitize($input)
     {
         return $input;
     }
@@ -28,16 +41,16 @@ class CptCallbacks {
      * @param mixed $args
      * @return  void
      */
-    public function textField( $args )
+    public function textField($args)
     {
-       
+
         $name = $args['labels_for'];
 
         $option_name = $args['option_name'];
 
         $title = $args['title'];
 
-        echo '<input type="text" name="'.$name.'" id="'.$name.'" placeholder="'.$title.'">';
+        echo '<input type="text" name="' . $name . '" id="' . $name . '" placeholder="' . $title . '">';
     }
 
     /**
@@ -46,9 +59,15 @@ class CptCallbacks {
      * return void
      */
 
-    public function testAreaField()
+    public function testAreaField($args)
     {
+        $name = $args['labels_for'];
 
+        $option_name = $args['option_name'];
+
+        $title = $args['title'];
+
+        echo '<textarea  name="' . $name . '" id="' . $name . '" placeholder="' . $title . '" rows="4" cols="18"> </textarea>';
     }
 
     /**
@@ -56,18 +75,16 @@ class CptCallbacks {
      * @param mixed $args
      * @return void
      */
-    public function checkboxField( $args )
+    public function checkboxField($args)
     {
         $name = $args['labels_for'];
         $classes = $args['class'];
         $toggle  = $args['data-toggle'];
         $option_name = $args['option_name'];
 
-        $checkbox = get_option( $option_name );
+        $checkbox = get_option($option_name);
 
-         echo '<input type="checkbox" class="'.$classes.'" id = "'.$name.'" name = "'.$option_name.'['.$name.']" 
-         value="1" '.($checkbox[$name] ? 'checked' : '').' data-toggle="'.$toggle.'">';
+        echo '<input type="checkbox" class="' . $classes . '" id = "' . $name . '" name = "' . $option_name . '[' . $name . ']" 
+         value="1" ' . ($checkbox[$name] ? 'checked' : '') . ' data-toggle="' . $toggle . '">';
     }
-
-
 }
